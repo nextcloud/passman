6e25bddac90cf8a8293129a0fdf081ff5fb51f20
brantje <brantje@gmail.com>
2016-09-10 15:26:51 +0200

    Add sass task to default grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,6 +69,6 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-html2js');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	// Default task(s).
-	grunt.registerTask('default', ['html2js']);
+	grunt.registerTask('default', ['html2js', 'sass']);
 
 };
