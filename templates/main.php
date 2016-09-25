52709971ca1f85cdb3d0cdb1722fd1c6922a801b
brantje <brantje@gmail.com>
2016-09-26 00:30:38 +0200

    Order tag list alphabetically

--- templates/main.php
+++ templates/main.php
@@ -96,7 +96,7 @@ style('passman', 'app');
 				</a>
 			</li>
 
-			<li ng-repeat="tag in available_tags" ng-if="selectedTags.indexOf(tag) == -1">
+			<li ng-repeat="tag in available_tags | orderBy:'text'" ng-if="selectedTags.indexOf(tag) == -1">
 				<a ng-click="tagClicked(tag)">{{tag.text}}</a>
 			</li>
 			<li data-id="trashbin" class="nav-trashbin">
