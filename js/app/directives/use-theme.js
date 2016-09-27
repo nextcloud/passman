2eb46ed9d7a4b5be0906dfc1c0e19b042d46563a
brantje <brantje@gmail.com>
2016-09-28 00:46:51 +0200

    Update use theme to create a font color

--- js/app/directives/use-theme.js
+++ js/app/directives/use-theme.js
@@ -8,17 +8,34 @@
  */
 angular.module('passmanApp')
 	.directive('useTheme', ['$window', function ($window) {
+
+		function invertColor(hexTripletColor) {
+			var color = hexTripletColor;
+			color = color.substring(1);           // remove #
+			color = parseInt(color, 16);          // convert to integer
+			color = 0xFFFFFF ^ color;             // invert three bytes
+			color = color.toString(16);           // convert to hex
+			color = ("000000" + color).slice(-6); // pad with leading zeros
+			color = "#" + color;                  // prepend #
+			return color;
+		}
 		return {
 			restrict: 'A',
 			scope:{
-				type: '=type'
+				type: '=type',
+				color: '='
 			},
 			link: function (scope, el, attr, ctrl) {
 				var _color = $('#header').css('background-color');
+
 				if(!scope.type) {
 					$(el).css('background-color', _color);
 				} else {
 					$(el).css(scope.type, _color);
+				}
+				if(scope.color){
+
+					$(el).css('color', invertColor(_color));
 				}
 			}
 		};
