9c53219346bb4c9b69baa27d69338d0b8b158a93
Marcos Zuriaga <wolfi@wolfi.es>
2016-10-11 23:15:45 +0200

    Removed unused method and variable

--- lib/Db/SharingACLMapper.php
+++ lib/Db/SharingACLMapper.php
@@ -18,21 +18,8 @@ use OCA\Passman\Utility\Utils;
 class SharingACLMapper extends Mapper {
     const TABLE_NAME = '*PREFIX*passman_sharing_acl';
 
-    public function __construct(IDBConnection $db, Utils $utils) {
+    public function __construct(IDBConnection $db) {
         parent::__construct($db, 'passman_sharing_acl');
-        $this->utils = $utils;
-    }
-
-    /**
-     * Gets all the credential data for the given user
-     * @param $userId
-     * @param $item_guid
-     * @return SharingACL[]
-     */
-    public function getCredentialPermissions(IUser $userId, $item_guid){
-        $sql = "SELECT * FROM ". self::TABLE_NAME ." WHERE user_id = ? AND item_guid = ?";
-
-        return $this->findEntities($sql, [$userId, $item_guid]);
     }
 
     public function createACLEntry(SharingACL $acl){
---
+++ tests/unit/lib/Db/SharingACLMapperTest.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ *
+ * Date: 11/10/16
+ * Time: 22:40
+ *
+ * @copyright Copyright (c) 2016, Marcos Zuriaga Miguel (wolfi@wolfi.es)
+ * @license AGPLv3
+ */
+class SharingACLMapperTest {
+
+}
