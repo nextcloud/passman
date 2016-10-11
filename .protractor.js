568f6e783111fcb1aedb9f88c1d72e22d0753e0d
brantje <brantje@gmail.com>
2016-10-11 22:25:50 +0200

    Update protractor config

--- .protractor.js
+++ .protractor.js
@@ -3,5 +3,11 @@ exports.config = {
 	specs: ['tests/js/create_vault.js'],
 	capabilities: {
 		'browserName': 'firefox' // or 'safari'
-	}
+	},
+	onPrepare: function() {
+		var SpecReporter = require('jasmine-spec-reporter');
+		// add jasmine spec reporter
+		jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'all'}));
+	},
+	print: function() {}
 };
