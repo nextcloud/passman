1db66284189777264a6488130118b05f32cc2ff1
brantje <brantje@gmail.com>
2016-10-11 23:46:25 +0200

    Update protractor config

--- .protractor.js
+++ .protractor.js
@@ -3,20 +3,16 @@ exports.config = {
 	specs: ['tests/js/create_vault.js'],
 	capabilities: {
 		'browserName': 'firefox', // or 'safari'
-		'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
 		'build': process.env.TRAVIS_BUILD_NUMBER
 	},
 	framework: 'jasmine',
-	/*onPrepare: function () {
+	onPrepare: function () {
 		var SpecReporter = require('jasmine-spec-reporter');
 		// add jasmine spec reporter
 		jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'all'}));
-	},*/
+	},
 	jasmineNodeOpts: {
-		showColors: true,
-		isVerbose: true,
-		realtimeFailure: true,
-		includeStackTrace: true,
+		silent: true,
 		defaultTimeoutInterval: 30000
 	}
 
