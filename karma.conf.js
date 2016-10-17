e84da75f05d4b8e7d1da1d7bb40b5a15de1c5e71
brantje <brantje@gmail.com>
2016-10-17 11:28:48 +0200

    Add required files for karma

---
+++ karma.conf.js
@@ -0,0 +1,78 @@
+// Karma configuration
+// Generated on Mon Oct 17 2016 15:46:52 GMT+0200 (CEST)
+var isTravis = (process.env.TRAVIS_BUILD_NUMBER) ? true : false;
+var browsers = ['Firefox'];
+if(!isTravis){
+	browsers.push('Chrome')
+}
+module.exports = function (config) {
+	config.set({
+
+		// base path that will be used to resolve all patterns (eg. files, exclude)
+		basePath: '.',
+
+
+		// frameworks to use
+		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+		frameworks: ['jasmine'],
+
+
+		// list of files / patterns to load in the browser
+		files: [
+			'tests/unit/js/mocks/*.js',
+			'../../core/vendor/jquery/dist/jquery.js',
+			'../../core/vendor/underscore/underscore.js',
+			'js/vendor/angular/angular.min.js',
+			'js/vendor/angular-mocks/angular-mocks.js',
+			'js/vendor/**/*.js',
+			'js/app/**/*.js',
+			{ pattern: 'tests/unit/js/app/**/*.js', included: true }
+		],
+
+
+		// list of files to exclude
+		exclude: [
+			'js/vendor/angular-mocks/ngAnimateMock.js',
+			'js/vendor/angular-mocks/ngMock.js',
+			'js/vendor/angular-mocks/ngMockE2E.js'
+		],
+
+
+		// preprocess matching files before serving them to the browser
+		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+		preprocessors: {},
+
+
+		// test results reporter to use
+		// possible values: 'dots', 'progress'
+		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+		reporters: ['verbose'],
+
+
+		// web server port
+		port: 9876,
+
+
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
+
+
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_INFO,
+
+
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: false,
+
+
+		// start these browsers
+		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+		browsers: browsers,
+
+
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: true
+	})
+};
---
+++ tests/unit/js/mocks/global.js
@@ -0,0 +1,1 @@
+var oc_requesttoken = null;
