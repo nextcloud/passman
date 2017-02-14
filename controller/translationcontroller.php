11eac75668f6ac67e5457db107dba454f94a76bd
brantje <brantje@gmail.com>
2017-02-14 13:28:51 +0100

    Indicate that sharing only works with users that have 1 or more vaults. Fixes #242

--- controller/translationcontroller.php
+++ controller/translationcontroller.php
@@ -226,6 +226,7 @@ class TranslationController extends ApiController {
 
 			// templates/vieuws/partials/forms/share_credential/basics.html
 			'search.u.g' => $this->trans->t('Search users or groups...'),
+			'search.result.missing' => $this->trans->t('Missing users? Only users that have vaults are shown.'),
 			'cyphering' => $this->trans->t('Cyphering'),
 			'uploading' => $this->trans->t('Uploading'),
 			'user' => $this->trans->t('User'),
