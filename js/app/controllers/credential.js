39bddd1e3c86d6ee28fc983a5c3f51191fcebae3
fnuesse <felix.nuesse@t-online.de>
2018-11-04 13:37:27 +0100

    fixed disappearing credentials on show-all
    Signed-off-by: fnuesse <felix.nuesse@t-online.de>

--- js/app/controllers/credential.js
+++ js/app/controllers/credential.js
@@ -439,7 +439,7 @@
                     var creds_filtered=[];
 
                     for (var i = 0; i < $scope.active_vault.credentials.length; i++) {
-                        if($scope.active_vault.credentials[i].delete_time>0){
+                        if($scope.active_vault.credentials[i].delete_time===0){
                             creds_filtered.push($scope.active_vault.credentials[i]);
                         }
                     }
