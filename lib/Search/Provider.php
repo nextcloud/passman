66f315061922323b8d13051ba3ab782cd76b2c02
binsky <timo@binsky.org>
2021-03-16 17:17:35 +0100

    remove named parameters as these are only supported from php7.4; set php7.2 as min php version

--- lib/Search/Provider.php
+++ lib/Search/Provider.php
@@ -42,10 +42,10 @@ use OCP\Search\SearchResultEntry;
 
 class Provider implements IProvider {
 
-	private IL10N $l10n;
-	private IURLGenerator $urlGenerator;
-	private IDBConnection $db;
-	private SettingsService $settings;
+	private $l10n;
+	private $urlGenerator;
+	private $db;
+	private $settings;
 
 	public function __construct(IL10N $l10n, IURLGenerator $urlGenerator, IDBConnection $db, SettingsService $settings) {
 		$this->l10n = $l10n;
