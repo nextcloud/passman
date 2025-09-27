0e5a9d2f14aec25571d5187302f9aac4b603e847
binsky <timo@binsky.org>
2025-09-27 23:32:29 +0200

    add .editorconfig to match Nextcloud's default file formatting
    Signed-off-by: binsky <timo@binsky.org>

--- rector.php
+++ rector.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 /**
  * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
@@ -9,14 +10,14 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-    ->withPaths([
-        __DIR__ . '/appinfo',
-        __DIR__ . '/lib',
-        __DIR__ . '/templates',
-        __DIR__ . '/tests',
-    ])
-    ->withPhpSets(php84: true)
-    ->withTypeCoverageLevel(0)
+	->withPaths([
+		__DIR__ . '/appinfo',
+		__DIR__ . '/lib',
+		__DIR__ . '/templates',
+		__DIR__ . '/tests',
+	])
+	->withPhpSets(php84: true)
+	->withTypeCoverageLevel(0)
 	->withSkip([
 		// skip rule since it marks code that's intended, as it is
 		\Rector\Php70\Rector\FuncCall\RandomFunctionRector::class,
